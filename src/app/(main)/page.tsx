249d083bd9ef24dcd97d3c0d8f979006800cb3f5
Muhammad Zain-ul-Abdin Ansari <zainzabid4338@gmail.com>
2024-06-25 18:47:13 +0500

    starter kit with next.js and tailwindcss

---
+++ src/app/(main)/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center gap-3 p-24">
+      <h1 className="text-5xl font-bold tracking-tighter">Starter Kit</h1>
+      <h1 className="text-5xl font-bold tracking-tighter">
+        Next.js + Tailwind CSS
+      </h1>
+    </main>
+  );
+}
---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Starter Kit",
+  description: "Developed by ~ Zain-ul-Abdin",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <main className="antialiased">{children}</main>
+      </body>
+    </html>
+  );
+}
